a37de9786b0eaf473cfac771ebd4caaf04c5e7de
Anwesh43 <anweshthecool0@gmail.com>
2020-05-31 17:12:41 +0530

    adding constants and libraries

--- flashydroppingballview/src/main/java/com/anwesh/uiprojects/flashydroppingballview/FlashyDroppingBallView.kt
+++ flashydroppingballview/src/main/java/com/anwesh/uiprojects/flashydroppingballview/FlashyDroppingBallView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.flashydroppingballview
 /**
  * Created by anweshmishra on 31/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val parts : Int = 5
+val scGap : Float = 0.05f / parts
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
