14fbe6c964eb437beda750c5d1337706198422a6
Anwesh43 <anweshthecool0@gmail.com>
2020-05-31 17:22:52 +0530

    adding FlashyDroppingBallView class

--- flashydroppingballview/src/main/java/com/anwesh/uiprojects/flashydroppingballview/FlashyDroppingBallView.kt
+++ flashydroppingballview/src/main/java/com/anwesh/uiprojects/flashydroppingballview/FlashyDroppingBallView.kt
@@ -50,3 +50,19 @@ fun Canvas.drawFDBNode(i : Int, scale : Float, paint : Paint) {
     drawFlashyDroppingBall(scale, size, h, paint)
     restore()
 }
+
+class FlashyDroppingBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
